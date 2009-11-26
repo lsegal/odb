699a5c9622c6c161cd7e6bfd2b7ea1c811524969
Loren Segal <lsegal@soen.ca>
2009-11-26 18:42:22 -0500

    Add spec for complex object

--- spec/persistence_spec.rb
+++ spec/persistence_spec.rb
@@ -1,4 +1,5 @@
 require File.dirname(__FILE__) + "/../lib/odb"
+require 'yard'
 
 class Post
   include ODB::Persistent
@@ -14,15 +15,24 @@ end
 
 describe ODB::Persistent do
   it "should save a post" do
-    db = ODB.new
+    db = ODB.new(ODB::JSONStore.new("Hello"))
     post = Post.new.tap {|p| p.title = "x"; p.author = "Joe"; p.comment = Comment.new }
     db.transaction do
       db.store[:post] = post
       db.store[:comment] = post.comment
     end
 
+    db = ODB.new(ODB::JSONStore.new("Hello"))
     db.store[:post].should == post
     db.store[:post].comment.object_id.should == db.store[:comment].object_id
+  end
+  
+  it "should save a complex object" do
+    YARD.parse(File.dirname(__FILE__) + '/../lib/**/*.rb')
+    db = ODB.new(ODB::JSONStore.new("yard"))
+    db.transaction do
+      db.store[:registry] = YARD::Registry.instance
+    end
   end
 end
 
